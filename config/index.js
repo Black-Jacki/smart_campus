966a514651a1cf8ca0201340980dff55bada3b2b
black-jacki <745644582@qq.com>
2020-10-30 20:25:27 +0800

    修改打包路径

--- config/index.js
+++ config/index.js
@@ -38,11 +38,11 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dist/smart_campus/index.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'static/smart_campus',
     assetsPublicPath: '/',
 
     /**
